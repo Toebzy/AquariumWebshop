946e0a5418167792071357b0bf5c09b545a87336
Stevespecious <g4m3rkc@gmail.com>
2023-12-13 16:46:31 +0100

    +

--- AquariumWebshop/src/pages/FishPage.jsx
+++ AquariumWebshop/src/pages/FishPage.jsx
@@ -3,6 +3,21 @@ function FishPage({isAdmin}) {
         <div id="subpage">
         <h1>Fish page</h1>
         <p>FishPage</p>
+
+        <li className="product-list_product-item">
+            <div className="product-card">
+                <div className="product-card_image">
+                    <img src="https://images.freeimages.com/fic/images/icons/2770/ios_7_icons/512/fish.png" alt="fish" width="100" height="100"></img>
+                </div>
+                <div className="product-card_info">
+                    <h2 className="product-card_info_title">Fish</h2>
+                    <p className="product-card_info_description">Fish</p>
+                    <p className="product-card_info_price">Price: 100</p>
+                    <button className="product-card_info_button">Add to cart</button>
+                </div>
+            </div>
+        </li>
+        
         </div>
     );
 }
