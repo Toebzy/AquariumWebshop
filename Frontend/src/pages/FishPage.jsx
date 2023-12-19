045b597d3c64d0e1451249b153abbcc87a13e1ff
SimoneToft <kagefighter123@gmail.com>
2023-12-19 22:18:48 +0100

    invertebrates

--- Frontend/src/pages/FishPage.jsx
+++ Frontend/src/pages/FishPage.jsx
@@ -10,7 +10,7 @@ function FishPage({ onProductAdd }) {
     { id: 2, name: 'Clownfish', text: '(Amphiprioninae)', price: '$15', image: 'src/assets/images/clownfish.jpg' },
     { id: 3, name: 'Royal Gramma', text: '(Gramma loreto)', price: '$25', image: 'https://freakincorals.com/cdn/shop/products/lg_72329_Royal_Gramma_Basslet_576x.jpg?v=1601919573' },
     { id: 4, name: 'Blue Tang', text: '(Paracanthurus hepatus)', price: '$30', image: 'src/assets/images/bluetang.jpeg' },
-    { id: 5, name: 'Garryfish', text: '(Garryfish)', price: '$4', image: 'src/assets/images/garryfish.png' },
+    { id: 5, name: 'Garryfish', text: '(Garryfish)', price: '$4', image: 'src/assets/images/garryfish.jpg' },
   ];
 
   
--- Frontend/src/pages/InvertebratesPage.jsx
+++ Frontend/src/pages/InvertebratesPage.jsx
@@ -7,11 +7,11 @@ function InvertebratesPage({ onProductAdd }) {
 
   // Sample invertebrate data (replace this with your actual data)
   const invertebrateData = [
-    { name: 'Angelfish', text: '(Pterophyllum scalare)', price: '$7', image: 'src/assets/images/angelfish.jpg' },
-    { name: 'Clownfish', text: 'Amphiprioninae', price: '$15', image: 'src/assets/images/clownfish.jpg' },
-    { name: 'Royal Gramma', text: 'Gramma loreto', price: '$25', image: 'src/assets/images/gramma.jpg' },
-    { name: 'Blue Tang', text: 'Paracanthurus hepatus', price: '$30', image: 'src/assets/images/bluetang.jpeg' },
-    { name: 'Garryfish', text: 'Garryfish', price: '$4', image: 'src/assets/images/garryfish.png' },
+    { name: 'Moon Jellyfish', text: '(Aurelia aurita)', price: '$20', image: 'https://www.eopugetsound.org/sites/default/files/styles/magazinewidth_592px/public/topical_article/images/moon_jellyfish.jpg?itok=Esreg6zX'},
+    { name: 'Anemone', text: '(Rose Bubble Tip)', price: '$140', image: 'https://static.wixstatic.com/media/f14946_e93a30d15ec24361805c00b3f721ddd1~mv2.jpg/v1/fill/w_2034,h_2036,al_c,q_85/f14946_e93a30d15ec24361805c00b3f721ddd1~mv2.jpg' },
+    { name: 'Snail', text: '(Astraea Turbo)', price: '$2', image: 'https://www.abyssaquatics.co.uk/wp-content/uploads/2020/10/100073-1.jpg' },
+    { name: 'Fire Blood Shrimp', text: '(Lysmata debelius)', price: '$65', image: 'https://www.oceanaquarium.com/wp-content/uploads/2012/12/35_fire_shrimp.jpg' },
+    { name: 'Blue Tuxedo Urchin', text: '(Mespilia sp.) ', price: '$29', image: 'https://www.discountcoral.com/cdn/shop/products/BlueTuxedoUrchin.jpg?v=1661358001' },
   ];
 
   // Filter the invertebrate data based on the searchQuery
