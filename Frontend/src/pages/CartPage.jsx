ab623dd33b4f4e4266a13dca750d11a5fe3654ce
Stevespecious <g4m3rkc@gmail.com>
2023-12-21 15:22:46 +0100

    +updated CartPage.jsx

--- Frontend/src/pages/CartPage.jsx
+++ Frontend/src/pages/CartPage.jsx
@@ -23,31 +23,35 @@ function CartPage({ isAdmin }) {
     }, []); // The empty dependency array ensures the effect runs only once when the component mounts
 
     return (
-        <div className="content">
-            <h1>CartPage</h1>
-            <p>This is for Users only.</p>
-            {cart && (
-                <div>
-                    <h2>Your Cart:</h2>
-                    <table className="cart-table">
-                        <thead>
-                            <tr className="cart-table_row">
-                                <th className="cart-table_header">Product Name</th>
-                                <th className="cart-table_header">Product Price</th>
-                            </tr>
-                        </thead>
-                        <tbody className="cart-table_section">
-                            {cart.cart_items.map((item) => (
-                                <tr key={item.id}>
-                                    <td className="cart-table_products">{item.productName}</td>
-                                    <td className="table_products">{item.productPrice}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-        </div>
+      <div className="content">
+        <h1>CartPage</h1>
+        <p>This is for Users only.</p>
+        {cart && (
+          <div>
+            <h2>Your Cart:</h2>
+            <table className="cart-table">
+              <thead>
+                <tr className="cart-table_row">
+                  <th className="cart-table_header">Product Name</th>
+                  <th className="cart-table_header">Product Price</th>
+                </tr>
+              </thead>
+              <tbody className="cart-table_section">
+                {cart.cart_items.map((item) => (
+                  <tr key={item.id}>
+                    <td className="cart-table_products_name">
+                      {item.productName}
+                    </td>
+                    <td className="cart-table_products_price">
+                      {item.productPrice}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     );
 }
 
